8e51ba87dae199b3f88446991981fbf6b575e15d
Brendon Reimer <130867372+zerosumhippo@users.noreply.github.com>
2024-11-22 07:02:28 -0600

    mars fleet shell script

---
+++ shell_scripts/mars_fleet/v_oneview_suppressive_fire.sql
@@ -0,0 +1,29 @@
+DROP VIEW IF EXISTS v_oneview_suppressive_fire;
+CREATE VIEW v_oneview_suppressive_fire
+AS
+SELECT field1
+  , field2
+  , field3
+  , field4
+FROM table1
+
+UNION ALL
+
+SELECT field1
+  , field2
+  , field3
+  , field4
+FROM table2
+  
+UNION ALL
+
+SELECT field1
+  , field2
+  , field3
+  , field4
+FROM table3
+;
+
+-- GRANT SELECT ON v_oneview_suppressive_fire
+-- TO <reporting_user>
+-- ;
