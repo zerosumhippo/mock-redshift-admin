31b95064a8fcb51d9e93e65635da4e9c1477fb41
Brendon Reimer <brendonsreimer@gmail.com>
2024-12-01 10:44:35 -0600

    eds oneview

---
+++ clients/starship_agency/ed_s_bomb_depot/v_oneview_viva_earth.sql
@@ -0,0 +1,11 @@
+DROP VIEW IF EXISTS ed_s_bomb_depot.v_oneview_viva_earth;
+CREATE VIEW ed_s_bomb_depot.v_oneview_viva_earth
+AS
+SELECT field1
+  , field2
+FROM table2
+;
+
+-- GRANT SELECT ON ed_s_bomb_depot.v_oneview_viva_earth
+-- TO <reporting_user>
+-- ;
